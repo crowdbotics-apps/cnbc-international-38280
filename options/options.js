10d985a404d8f710c38f3e3f359219e81d4b6343
Crowdbotics <team@crowdbotics.com>
2023-01-12 20:08:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cnbc-international-38280",
+  url: "https://cnbc-international-38280.botics.co",
+  api: "https://cnbc-international-38280.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
